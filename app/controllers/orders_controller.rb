f0d7f989957705d29cdfec7841cce9e2c541b9ad
MarcosLorejan <marcos.lorejan@gmail.com>
2022-07-31 10:36:33 -0300

    Validation!

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,21 @@ class OrdersController < ApplicationController
   before_action :ensure_cart_isnt_empty, only: :new
   before_action :set_order, only: %i[ show edit update destroy ]
 
+  def pay_type_params
+    if order_params[:pay_type] == "Credit card"
+      params.require(:order).permit(:credit_card_number, :expiration_date)
+  
+    elsif order_params[:pay_type] == "Check"
+    params.require(:order).permit(:routing_number, :account_number)
+
+    elsif order_params[:pay_type] == "Purchase order"
+    params.require(:order).permit(:po_number)
+
+    else
+      {}
+      
+    end
+  end
 
   # GET /orders or /orders.json
   def index
