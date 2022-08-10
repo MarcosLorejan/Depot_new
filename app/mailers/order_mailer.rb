e249c96002aad01e0bc866d9d475ca2ec0995000
MarcosLorejan <marcos.lorejan@gmail.com>
2022-08-09 21:25:31 -0300

    Validation!

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,5 +1,14 @@
+#---
+# Excerpted from "Agile Web Development with Rails 6",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
+#---
 class OrderMailer < ApplicationMailer
-  default from: "Marcos Lorejan <marcos.lorejan@gmail.com>"
+  default from: 'Sam Ruby <depot@example.com>'
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -8,7 +17,7 @@ class OrderMailer < ApplicationMailer
   def received(order)
     @order = order
 
-    mail to: order.email, subject: 'Order confirmation'
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -16,9 +25,9 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.shipped.subject
   #
-  def shipped
+  def shipped(order)
     @order = order
 
-    mail to: order.email, subject: 'Order Shipped'
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
   end
 end
